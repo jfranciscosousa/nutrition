e662b412d8f62065964db4bf169ebcc3d4361671
Francisco Sousa <francisco.sousa@hey.com>
2023-04-06 21:05:44 +0100

    :sunrise:

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { navigating } from '$app/stores';
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+</script>
+
+<main class="h-screen w-screen flex flex-col items-center justify-center">
+	{#if $navigating?.type === 'form'}
+		<ProgressRadial />
+	{:else}
+		<form class="flex flex-col items-center" action="/search">
+			<!-- svelte-ignore a11y-autofocus -->
+			<input
+				class="w-[440px] input p-2"
+				placeholder="eg: 100g of kidney beans"
+				name="query"
+				autofocus
+			/>
+			<button class="btn variant-filled-primary mt-4">Search</button>
+		</form>
+	{/if}
+</main>
---
+++ src/routes/search/+page.server.ts
@@ -0,0 +1,22 @@
+import { getFood } from '$lib/data/getFood';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ url, setHeaders }) => {
+	const query = url.searchParams.get('query');
+
+	if (!query)
+		return {
+			noResults: true
+		};
+
+	setHeaders({ 'cache-control': 's-maxage=5000' });
+
+	const food = await getFood(query);
+
+	if (!food)
+		return {
+			noResults: true
+		};
+
+	return { food };
+};
